9b58e0b7ade86a95920a8cfe82423534cd48589e
naveenvarma600 <naveenvarma600@gmail.com>
2022-09-20 18:05:01 +0530

    uploaded

---
+++ public/script.js
@@ -0,0 +1,82 @@
+let socket = io();
+
+// let boombtn = document.getElementByID('boom')
+// boombtn.onclick = function(){
+//     socket.emit('boom') //emitting event boom into socket
+// }
+
+// socket.on('whizz',function(){
+//     let div = document.createElement('div')
+//     div.innerText = 'whizz'
+//     document.body.appendChild(div)
+// })
+
+// let btnsend = document.getElementById('btnsend')
+// let inputmsg = document.getElementById('inputmsg')
+// let ulmsglist = document.getElementById('ulmsglist')
+
+// btnsend.onclick = function(){
+//     socket.emit('msgsend',{
+//         msg: inputmsg.value    //this is the data,w e get to server, alsong with emitted event
+//     })
+//     inputmsg.value
+// }
+
+// socket.on('msgrcv',(data)=>{
+//     let linewmsg = document.createElement('li')
+//     linewmsg.innerText = data.msg
+//     ulmsglist.appendChild(linewmsg)
+// })
+function generatecard(value){
+    let text = `<div class="card" style="width: 18rem;"> <div class="card-body"> <h5 class="card-title">${value}</h5> </div> </div>`;
+    return text;
+}
+
+$('#loginbox').show()
+$("#chatbox").hide()
+$('#btnstart').click(()=>{
+    socket.emit('login',{
+        username:$('#username').val(),
+        password:$('#password').val()
+    })
+})
+
+socket.on('loggedin',(users)=>{
+    $('#loginbox').hide()
+    $("#chatbox").show()
+    let text="";
+    users["users"].forEach((value)=>{text+="<li>"+generatecard(value)+"</li>";});
+    $("#available_list").html(text);
+})
+
+$("#btnsend").click(()=>{
+    socket.emit('msgsend',{
+        to: $("#inputtouser").val(),
+        msg:$('#inputnewmsg').val()
+    })
+})
+
+socket.on('msgrcv',(data1)=>{
+    if(data1.mode=='singleto'){
+        $('#ulmsgs').append($('<li>').text(
+            `[${data1.data.from} (PRIVATE)] : ${data1.data.msg}`
+        ))
+    }
+    else if(data1.mode=='singlefrom'){
+        $('#ulmsgs').append($('<li>').text(
+            `[${data1.data.to} (PRIVATE)] : ${data1.data.msg}`
+        ))
+    }
+    else{
+        $('#ulmsgs').append($('<li>').text(
+            `[${data1.data.from} (PUBLIC)] : ${data1.data.msg}`
+        ))
+    }
+})
+
+
+socket.on('loginfalied',()=>{
+    window.alert('wrong username or password')
+    $('#loginbox').show()
+    $("#chatbox").hide()
+})
---
+++ server.js
@@ -0,0 +1,80 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+
+const server = http.createServer(app);
+const io = socketio(server)  //cant directly send express, we nee dto sedn html server
+
+// io.on('connection',(socket)=>{
+//     console.log('connected with socket id = ',socket.id);
+//     socket.on('boom',()=>{   //whenever boom event is called, written in script.js
+//         console.log('boom received from',socket,id)
+//     })
+// })
+
+let users = {   //username and password
+    
+}
+let socketmap = {
+
+}
+
+io.on('connection',(socket)=>{
+    console.log('connected with socket id = ',socket.id);
+    // socket.on('msgsend',(data)=>{   //whenever boom event is called, written in script.js
+    //     // console.log('msg received ',data.msg)
+    //     // socket.emit -> to same func itself
+    //     io.emit('msgrcv',data)  // EMIT ON IO, NOT ON SOCKET, ( SOCKET WILL  SEND TO THAT CHANNEL ONLY , ** IO WILL SEND TO ALL CHANNELS ITS OCNNECTED)
+    //     // socket.broadcast.emit('msgrcv',data)  //this event is sended to every other event , expcet current one
+    // })
+    function login(s,u){
+        s.join(u)
+        // s.emit("loggedin",{users:Object.keys(users)})
+        io.emit("loggedin",{users:Object.keys(users)})
+        socketmap[s.id] = u   //mapping socket id with username
+    }
+    socket.on('login',(data)=>{
+        if(users[data.username]){
+            if(users[data.username] == data.password){    //is user is found and password matched
+                login(socket,data.username)
+            }
+            else{
+                socket.emit('loginfalied')
+            }
+        }
+        else{
+            users[data.username] = data.password   //creating new users
+            login(socket,data.username)
+        }
+        // socket.join(data.username)    //joining socket with username
+        // socket.emit("loggedin",data)
+        
+    })
+    socket.on('msgsend',(data)=>{
+        data.from = socketmap[socket.id]   //finding name of that socketID
+        if(data.to){
+            io.to(data.to).emit('msgrcv',{data:data,
+                mode:'singleto'
+            }) //as in line 26, joined username to socket, we can call io.to(ussername)//for that socket
+            io.to(data.from).emit('msgrcv',{data:data,
+                mode:'singlefrom'
+            })
+        }
+        else{
+            io.emit('msgrcv',{data:data,
+                mode:'all'
+            })
+        }
+    })
+})
+
+
+app.use('/',express.static(__dirname + '/public'))
+
+server.listen(8000, ()=>{
+    console.log("staretd in localhost:8000")
+})
+
+//cmd -> ipconfig -> ipv4
